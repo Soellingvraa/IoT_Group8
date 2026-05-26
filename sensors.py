630b7af475bd84c2a901ce629b6552cc9575cdd2
JesperLaur <95864826+JesperLaur@users.noreply.github.com>
2026-05-26 11:15:58 +0200

    final cleanup

--- sensors.py
+++ sensors.py
@@ -1,7 +1,6 @@
 #sensors.py
 #sense hat interaction logic
-#This module should be responsible for interacting with the sense hat and pulling data from it, 
-# it is a service that is used by the raspberry pi 2, that only pulls data from the sensehat while another script is responsible for uploading the data to rabbitMQ
+
 
 from sense_hat import SenseHat
 sense = SenseHat()
